73883b4f54bfa05ceeb89b653ffdf031e28e66c6
gusdn0108 <gusdn6671@naver.com>
2022-05-17 10:44:17 +0900

    흠

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer"
 import Container from "./components/common/Container";
-import Grid from '@mui/material/Grid'
-import Login from './components/pages/Login'
-import index from "./components/pages";
-import { Link } from "react-router-dom";
-import Navbar from "./components/common/Navbar";
 import Responsive from "./components/common/Responsive";
-import Article from './components/common/Article'
-import { Box } from "@mui/system";
-import BoxSx from "./components/common/Box";
 import Signup from "./components/pages/Signup";
 import ImageSilder from "./components/common/imageSilder";
 
